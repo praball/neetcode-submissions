35b3892d2cb6c2bcc1f3b40efcf4e709bdbc4496
Prabal Pophaley <92200181+praball@users.noreply.github.com>
2026-08-17 18:57:25 +0530

    Add: count-number-of-islands - submission-1

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-1.cpp
@@ -0,0 +1,37 @@
+class Solution {
+   public:
+
+    vector<vector<int>> vis = vector<vector<int>>(101, vector<int>(101, 0));
+    // set all to not visited
+    int res = 0, n = 0, m = 0;
+
+    int numIslands(vector<vector<char>>& grid) {
+        n = grid.size(), m = grid[0].size();
+        for (int i = 0; i < grid.size(); i++) {
+            for (int k = 0; k < grid[0].size(); k++) {
+                if (grid[i][k] == '0')
+                    continue;
+                else {                     // if possible dfs available
+                    if (vis[i][k] == 0) {  // if unvisited, dfs/island available
+                        dfs(grid, i, k);
+                        res++;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+    void dfs(vector<vector<char>>& grid, int i, int k) {
+        if (i >= n || i < 0 || k < 0 || k >= m) return;  // Out of bounds
+        if (vis[i][k] == 1) return; // already visited
+        if (grid[i][k] == '0') return;
+        else {
+            vis[i][k] = 1;
+            dfs(grid, i + 1, k);
+            dfs(grid, i, k + 1);
+            dfs(grid, i - 1, k);
+            dfs(grid, i, k - 1);
+        }
+    }
+};
